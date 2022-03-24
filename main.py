b328d9b0030dac22591a370f2c4023b6778602a8
joaor1os <101368524+joaor1os@users.noreply.github.com>
2022-03-24 17:29:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask('__main__')
+
+@app.route('/')
+def hello_world():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran')
+def unifran():
+    return '<h2>Universidade de Franca</h2>'
+
+@app.route('/dashboard/<name>')
+def dashboard(name):
+    return f'Olá, {name}'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
